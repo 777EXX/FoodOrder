802c0ef7faf51a106e3c0a8d36f11db13f0f50d9
777EXX <panda3er@gmail.com>
2023-11-10 16:49:47 +0300

    Initial Commit

---
+++ FoodOrder/FoodOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodOrderApp.swift
+//  FoodOrder
+//
+//  Created by MacBook on 10.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
